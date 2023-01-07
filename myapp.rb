b11c1367562f13153bad803515cdd02718a03309
3rick <erick.dejesus1@alumno.buap.mx>
2023-01-07 14:14:15 -0600

    add: my_app, views sinatra web app

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,13 @@
 # myapp.rb
 require 'sinatra'
+require_relative 'lib/code_breaker.rb'
 
 get '/' do
  erb :index
 end
+post '/' do
+    play = CodeBreaker.new("7519")
+    guess = play.try (params[:guess])
+    " your try was , #{guess}!"
+  end
+  
