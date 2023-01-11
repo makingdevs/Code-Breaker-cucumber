0573514d0c93f78e813d9dcbbd031aeae210b440
3rick <erick.dejesus1@alumno.buap.mx>
2023-01-11 12:52:23 -0600

    refactor: method post

--- myapp.rb
+++ myapp.rb
@@ -3,11 +3,20 @@ require 'sinatra'
 require_relative 'lib/code_breaker.rb'
 
 class MyApp < Sinatra::Base
+  enable :sessions
   get '/' do
-    play = CodeBreaker.new("7519")
-    guess = play.try (params[:guess]) unless params[:guess].nil?
-    win = play.win
+    result = session[:result]
+    guess = result[:guess] unless result.nil?
+    win = result[:win] unless result.nil?
     erb :index, :locals => {:guess => guess, :win => win}
+  end
+  post '/' do
+    play = CodeBreaker.new("7519")
+    guess = play.try(params[:guess])
+    win = play.win
+    result = {:guess => guess, :win => win}
+    session[:result] = result
+    redirect '/'
   end
   run! if app_file == $0
 end
