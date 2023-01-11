1083ad2988f230f4097a340c995e4790342a9547
3rick <erick.dejesus1@alumno.buap.mx>
2023-01-11 08:17:47 -0600

    update: run directly, heritage from Sinatra::Base

--- myapp.rb
+++ myapp.rb
@@ -2,9 +2,11 @@
 require 'sinatra'
 require_relative 'lib/code_breaker.rb'
 
-get '/' do
-  play = CodeBreaker.new("7519")
-  guess = play.try (params[:guess])
-  guess = params[:guess]
-  erb :index, :locals => {:guess => guess}
+class MyApp < Sinatra::Base
+  get '/' do
+    play = CodeBreaker.new("7519")
+    guess = play.try (params[:guess]) unless params[:guess].nil?
+    erb :index, :locals => {:guess => guess}
+  end
+  run! if app_file == $0
 end
