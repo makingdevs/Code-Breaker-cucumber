114b4efa2b5926a5fa2112795bf517144e0338d9
3rick <erick.dejesus1@alumno.buap.mx>
2023-01-15 19:47:15 -0600

    add: start and stop server by run `cucumber`
    delete all cookies in each scenario

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,71 +1,44 @@
-ENV['RACK_ENV'] = 'test'
-require File.join(File.dirname(__FILE__), '../..', 'myapp.rb')
-
-require 'capybara/cucumber'
+require_relative '../../myapp.rb'
 require 'rubygems'
 require 'selenium-cucumber'
 
-Capybara.app = MyApp
-Capybara.server = :puma
-
-Before do
+BeforeAll do
   puts "Starting App..."
-  if $pid.nil?
-    $pid = fork do
-      MyApp.run!
-    end
+  $pid = fork do
+    MyApp.run!
   end
   sleep 1
   puts "Started App..."
 end
 
+AfterAll do
+  unless $pid.nil?
+    Process.kill "TERM", $pid
+    Process.wait $pid
+  end
+  sleep 1
+  puts "Started App..."
+end
+
+Before do
+  $driver.manage.delete_all_cookies
+end
+
 # Store command line arguments
 $browser_type = ENV['BROWSER'] || 'ff'
 $platform = ENV['PLATFORM'] || 'desktop'
-$os_version = ENV['OS_VERSION']
-$device_name = ENV['DEVICE_NAME']
-$udid = ENV['UDID']
 $app_path = ENV['APP_PATH']
 
 # check for valid parameters
 validate_parameters $platform, $browser_type, $app_path
 
 # If platform is android or ios create driver instance for mobile browser
-if $platform == 'android' or $platform == 'iOS'
-  
-  if $browser_type == 'native'
-    $browser_type = "Browser"
-  end
-  
-  if $platform == 'android'
-    $device_name, $os_version = get_device_info
-  end
-  
-  desired_caps = {
-    caps:       {
-      platformName:  $platform,
-      browserName: $browser_type,
-      versionNumber: $os_version,
-      deviceName: $device_name,
-      udid: $udid,
-      app: ".//#{$app_path}"
-      },
-    }
-
-  begin
-    $driver = Appium::Driver.new(desired_caps).start_driver
-  rescue Exception => e
-    puts e.message
-    Process.exit(0)  
-  end
-else # else create driver instance for desktop browser
-  begin
-    $driver = Selenium::WebDriver.for(:"#{$browser_type}")
-    $driver.manage().window().maximize()
-  rescue Exception => e
-    puts e.message
-    Process.exit(0)
-  end
+begin
+  $driver = Selenium::WebDriver.for(:"#{$browser_type}")
+  $driver.manage().window().maximize()
+rescue Exception => e
+  puts e.message
+  Process.exit(0)
 end
 
 at_exit do
