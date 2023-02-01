dfc8dc72f15aad6a2f2d12a1e0b8c3d7cbbca5a5
Hector Garcia Rosales <ghectormus@gmail.com>
2023-02-01 11:09:21 -0600

    add: changing for a Background scenario

--- myapp.rb
+++ myapp.rb
@@ -1,32 +1,43 @@
-# myapp.rb
 require 'sinatra'
 require_relative 'lib/code_breaker.rb'
 
 class MyApp < Sinatra::Base
   enable :sessions
   get '/' do
-    result = session[:result]&.last
-    @results = session[:result]
-    @guess = result[:guess] unless result.nil?
-    @win = result[:win] unless result.nil?
-    @attemps = result[:attemps] unless result.nil?
-    erb :index
+    if session[:name].nil?
+      erb :player_name
+    else
+      result = session[:result]&.last
+      @results = session[:result]
+      @guess = result[:guess] unless result.nil?
+      @win = result[:win] unless result.nil?
+      @attemps = result[:attemps] unless result.nil?
+      @name = session[:name]
+      erb :index
+    end
   end
+
   post '/' do
-    play = CodeBreaker.new("7519")
-    play.attemps = session[:result].last[:attemps] if session[:result]
-    redirect '/' if play.attemps == 0 
-    guess = play.try(params[:guess])
-    win = play.win
-    result = session[:result] ? session[:result] : []
-    result << {
-      :guess => guess,
-      :attemp => params[:guess],
-      :win => win,
-      :attemps => play.attemps
-    }
-    session[:result] = result
-    redirect '/'
+    if session[:name].nil?
+      session[:name] = params[:name]
+      redirect '/'
+    else
+      play = CodeBreaker.new("7519")
+      play.attemps = session[:result].last[:attemps] if session[:result]
+      redirect '/' if play.attemps == 0 
+      guess = play.try(params[:guess])
+      win = play.win
+      result = session[:result] ? session[:result] : []
+      result << {
+        :guess => guess,
+        :attemp => params[:guess],
+        :win => win,
+        :attemps => play.attemps
+      }
+      session[:result] = result
+      redirect '/'
+    end
   end
+  
   run! if app_file == $0
 end
