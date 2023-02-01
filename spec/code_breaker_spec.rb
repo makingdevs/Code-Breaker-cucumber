af9821c03aec3fd5555fbc01601e5cd89e3790bd
Hector Garcia Rosales <ghectormus@gmail.com>
2023-02-01 11:07:19 -0600

    add: Test for new feature

--- spec/code_breaker_spec.rb
+++ spec/code_breaker_spec.rb
@@ -37,11 +37,5 @@ describe CodeBreaker do
             play = CodeBreaker.new("7519")
             expect(play.try("7519")).to eq "____"
         end
-    end
-    context "Shows the player name" do 
-        it "returns Default user as user" do 
-            play = CodeBreaker.new("1234")
-            expect(play.player).to eq("Default userssss")
-        end
-    end
+    end    
 end
