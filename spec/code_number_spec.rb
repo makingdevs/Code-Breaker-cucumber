98e9fdfe9682f611e47cdfa24bb3f9e7b477df3c
3rick <erick.dejesus1@alumno.buap.mx>
2023-02-06 22:46:18 -0600

    test: CodeNumber first digit shoul not be 0

--- spec/code_number_spec.rb
+++ spec/code_number_spec.rb
@@ -8,5 +8,8 @@ describe CodeNumber do
     it 'all its digits are different' do
       expect(CodeNumber.generate_code.chars.uniq.size).to eq 4
     end
+    it 'first digit shoul not be 0' do
+      expect(CodeNumber.generate_code[0]).not_to eq '0'
+    end
   end
 end
