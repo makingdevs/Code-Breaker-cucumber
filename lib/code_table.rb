1267fbc126ea308a99132ea6c6cdc57121e8a8fd
Hector Garcia Rosales <ghectormus@gmail.com>
2023-02-09 09:51:13 -0600

    add: Algorithm for table feature

---
+++ lib/code_table.rb
@@ -0,0 +1,15 @@
+require 'csv'
+class CodeTable 
+  def self.write_data(player_name, score)
+    CSV.open('resources/players_data.csv', 'a') do |csv|
+      csv << [player_name, score.to_i]
+    end
+  end
+
+  def self.prepare_file
+    unless File.exists?('resources/players_data.csv')
+      File.open('resources/players_data.csv', 'w') {|file| file.truncate(0)}
+    end
+  end
+  
+end
