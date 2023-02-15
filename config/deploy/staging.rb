2d0b25eb921247935de0661728b42902f13acf10
Hector Garcia Rosales <ghectormus@gmail.com>
2023-02-15 12:18:46 -0600

    fix: call to the server with expanded path

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -22,7 +22,7 @@ namespace :deploy do
     on roles(:all) do
       within "#{fetch :deploy_to }/current/" do
         info "**** Starting server ****"
-        execute :bundle, "exec rackup -p 9292 -P PID.pid --env #{fetch :enviroment} --daemonize "
+        execute :bundle, "exec rackup -p 9292 -P PID.pid --env #{fetch :enviroment} --daemonize --ExpandPath=true "
       end
     end
   end
